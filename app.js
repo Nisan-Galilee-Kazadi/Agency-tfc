84c49c592c2528149f91779f25878819b5d6a7ab
Nisan-Galilée-Kazadi <galileokazadi45@gmail.com>
2025-01-23 22:00:08 +0100

    accordion page menu

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-const corps = document.getElementById('corps')
-const title = document.getElementsByClassName('title')
+
+
+const liPages = document.querySelector('pages')
+
+const heroSection = document.querySelector('.introsection')
+
+function blurry() {
+    if(heroSection.classList.contains('blurry')){
+        heroSection.classList.remove('blurry')
+    }
+    else{
+        heroSection.classList.add('blurry')
+    }
+}
+
+blurry()
+
+liPages.addEventListener("mouseover",blurry)
+
+
+
+
