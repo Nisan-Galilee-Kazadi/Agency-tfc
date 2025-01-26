3ab3718f57950b55096a73b477c56fbbfd14e16f
Nisan-Galilee-Kazadi <galileokazadi45@gmail.com>
2025-01-26 01:28:39 +0100

    mise a jour 1

--- app.js
+++ app.js
@@ -15,9 +15,25 @@ function openMenu () {
 
 
 buttonMenu.addEventListener('click', openMenu)
-menuList.addEventListener('click', openMenu)
 
 openMenu()
 
+
+const menuOpen = document.getElementById('open-icon')
+
+const menuClose = document.getElementById('close-icon')
+
+const contenair = document.querySelector('.menu')
+
+contenair.addEventListener('click', function (){
+
+    if(menuOpen.style.display !== 'none'){
+        menuOpen.style.display = 'none';
+        menuClose.style.display = 'block';
+    }else{
+        menuOpen.style.display = 'block';
+        menuClose.style.display = 'none';
+    }
+});
 
 
