ffa3d24bddb45748e1d784d2e22990793fd8351f
Ton Nom <ton_email@example.com>
2025-03-27 11:01:15 +0100

    amelioration menu

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
+const buttonMenu = document.getElementById("menuIcon");
 
+const menuList = document.getElementById("menuList");
 
-const buttonMenu = document.getElementById('menuIcon')
-
-const menuList = document.getElementById('menuList')
-
-function openMenu () {
-    if(menuList.classList.contains('open')){
-        menuList.classList.remove('open')
-    }
-    else{
-        menuList.classList.add('open')
-    }
+function openMenu() {
+  if (menuList.classList.contains("open")) {
+    menuList.classList.remove("open");
+  } else {
+    menuList.classList.add("open");
+  }
 }
 
+buttonMenu.addEventListener("click", function () {
+  openMenu();
+  changeButton();
+});
 
-buttonMenu.addEventListener('click', function(){
-    openMenu();
-    changeButton();
-})
+const menuOpen = document.getElementById("open-icon");
+menuOpen.style.display = "none";
 
-openMenu()
+const menuClose = document.getElementById("close-icon");
+menuClose.style.display = "block";
 
-
-const menuOpen = document.getElementById('open-icon')
-
-const menuClose = document.getElementById('close-icon')
-
-const contenair = document.querySelector('.menu')
-
+const contenair = document.querySelector(".menu");
 
 function changeButton() {
-    if(menuOpen.style.display !== 'none'){
-        menuOpen.style.display = 'none';
-        menuClose.style.display = 'block';
-    }else{
-        menuOpen.style.display = 'block';
-        menuClose.style.display = 'none';
-    }
+  if (menuOpen.style.display !== "none") {
+    menuOpen.style.display = "none";
+    menuClose.style.display = "block";
+  } else {
+    menuOpen.style.display = "block";
+    menuClose.style.display = "none";
+  }
 }
 
+const navBar = document.getElementById("navBar");
 
+function scrollBar() {
+  if (window.scrollY >= 300) {
+    navBar.classList.add("scroller");
+  } else {
+    navBar.classList.remove("scroller");
+  }
+}
+
+window.addEventListener("scroll", scrollBar);
